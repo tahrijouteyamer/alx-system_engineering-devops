f464cd935fdc6ea12b5356f32dcff35030e86d2b
tahrijouteyamer <tahrijouteyamer@hotmail.com>
2023-11-29 03:06:15 -0800

    .

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+# A puppet manifest installing flask v2.1.0 which is a package from pip3.
+# I'm also specifying the version of flask to install using the ensure attribute
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => pip3,
+}
