e12c18758f3b73b0a285a71bdd6f426e17656068
tahrijouteyamer <tahrijouteyamer@hotmail.com>
2024-03-06 01:34:10 +0000

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """
-    Uses Reddit API to print the number of subscribers of a subreddit
+Uses Reddit API to print the number of subscribers of a subreddit
 """
 import requests
+import ssl  # Import the ssl module
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'CustomUserAgent/1.0'}
-    response = requests.get(url, headers=headers)
+    """
+    Get the number of subscribers for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        subreddit_info = response.json()
-        return subreddit_info['data']['subscribers']
-    elif response.status_code == 404:
+    if response.status_code != 200:
         return 0
+
+    data = response.json().get("data")
+    if data:
+        num_subs = data.get("subscribers")
+        # Use repr() to get a string representation of OPENSSL_VERSION
+        ssl_version = repr(ssl.OPENSSL_VERSION)
+        print("the 'ssl' module is compiled with {}.".format(ssl_version))  # Print the message
+        return num_subs
     else:
         return 0
 
 # Example usage:
-subreddit_name = "python"
-subscribers_count = number_of_subscribers(subreddit_name)
-
-if subscribers_count > 0:
-    print(f"The subreddit '{subreddit_name}' has {subscribers_count} subscribers.")
-else:
-    print(f"The subreddit '{subreddit_name}' is not valid or not found.")
+if __name__ == "__main__":
+    subreddit_name = "python"  # Replace with the desired subreddit name
+    subscribers_count = number_of_subscribers(subreddit_name)
+    print("Subscribers in r/{}: {}".format(subreddit_name, subscribers_count))
 
